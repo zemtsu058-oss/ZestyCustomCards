38e56f3a222d2bfee5d1977314dde0028e5a5725
zemtsu058-oss <zemtsu058@gmail.com>
2026-02-23 13:28:38 +0700

    Add files via upload

---
+++ script/constants.lua
@@ -0,0 +1,8 @@
+-- Custom Archetype
+SET_TTF                           = 0x789
+SET_ATERMIS                       = 0x780
+SET_CAT                           = 0x781
+SET_DESIRE_HERO                   = 0x927
+SET_BUCKLE                        = 0x315
+-- Custom counter
+COUNTER_MANA                      = 0x177
